338a712fe9d34b2b493a8de2863d7e6013ed08db
MoadElfatihi <34817694+MoadElfatihi@users.noreply.github.com>
2025-04-19 00:10:11 +0200

    set up spring batch project
    set up spring batch project

---
+++ src/main/java/com/batchkatas/batchkatas/BatchkatasApplication.java
@@ -0,0 +1,13 @@
+package com.batchkatas.batchkatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchkatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchkatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batchkatas/batchkatas/BatchkatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batchkatas.batchkatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchkatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
