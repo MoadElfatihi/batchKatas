e17ea330af8963ec1cd44c533b28a65c50be6c05
MoadElfatihi <34817694+MoadElfatihi@users.noreply.github.com>
2025-04-19 20:47:07 +0200

    Demonstrate use of Listener

---
+++ src/main/java/com/batchkatas/batchkatas/architecture/HelloWorldExecutionListener.java
@@ -0,0 +1,23 @@
+package com.batchkatas.batchkatas.architecture;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldExecutionListener implements JobExecutionListener {
+
+	@Override
+	public void beforeJob(JobExecution jobExecution) {
+		System.out.println("launching job="+jobExecution.getJobInstance().getJobName()+
+							" with parameters="+jobExecution.getJobParameters())
+						;
+		jobExecution.getExecutionContext().put( "name", " My name" );
+	}
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		System.out.println("After  job execute for greeting ="+jobExecution.getExecutionContext().get( "name" ));
+
+	}
+}
