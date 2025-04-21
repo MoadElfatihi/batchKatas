d4386e0a255b51e2eba440f2afc81f92aa9c517b
Moad <elfatihi.moad@gmail.com>
2025-04-21 18:29:14 +0200

    add another csv reader

--- src/main/java/com/batchkatas/batchkatas/csv/CsvBatchConfig.java
+++ src/main/java/com/batchkatas/batchkatas/csv/CsvBatchConfig.java
@@ -2,6 +2,7 @@ package com.batchkatas.batchkatas.csv;
 
 
 import com.batchkatas.batchkatas.model.ProductDto;
+import com.batchkatas.batchkatas.model.SupplierDto;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.Job;
 
@@ -11,6 +12,8 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,16 +31,43 @@ public class CsvBatchConfig {
 	private StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job csvJob(){
-		return jobBuilderFactory.get( "csvJob" )
+	public Job csvProductJob(){
+		return jobBuilderFactory.get( "csvProductJob" )
 				.incrementer( new RunIdIncrementer() )
 				.start( csvStep() )
+				.next( csvProductSupplierStep() )
 				.build();
 	}
 
 	@Bean
+	public Step csvProductSupplierStep(){
+		return stepBuilderFactory.get( "csvProductSupplier" )
+				.chunk( 3 )
+				.reader( productSupplierReader() )
+				.writer( new ProductConsoleItemWriter() )
+				.build();
+	}
+
+	@Bean
+	public ItemReader<SupplierDto> productSupplierReader(){
+		return new FlatFileItemReaderBuilder<SupplierDto>()
+				.name( "supplierReader" )
+				.resource( new FileSystemResource( "files/products_supplier.csv" ) )
+				.delimited()
+				.names( new String[]{"registrationNumber","lastName","firstName","position","birthYear","debutYear"} )
+				.linesToSkip( 1 )
+				.fieldSetMapper(
+						new BeanWrapperFieldSetMapper<SupplierDto>(){
+							{
+								setTargetType( SupplierDto.class );
+							}
+						}
+				)
+				.build();
+	}
+	@Bean
 	public Step csvStep(){
-		return stepBuilderFactory.get( "csvStep" )
+		return stepBuilderFactory.get( "csvProductStep" )
 				.chunk( 3 )
 				.reader(  productCsvReader())
 				.writer(  new ProductConsoleItemWriter())
